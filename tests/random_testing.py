17e6789351cac4902a5fb119b28f710f929b0344
Jackson Goerner <37640160+glipR@users.noreply.github.com>
2020-05-14 22:08:54 +1000

    Add basic objects and serialization.

--- tests/random_testing.py
+++ tests/random_testing.py
@@ -1,35 +1,54 @@
 import numpy as np
 
 from simulation.interactor import IInteractor
-from visual import ScreenObjectManager, Rectangle, Circle
+from objects.base import objectFactory
+from visual import ScreenObjectManager
 
 class RandomInteractor(IInteractor):
 
+    ROBOT_DEFINITION = {
+        'visual': {
+            'name': 'Circle',
+            'radius': 0.15,
+            'fill': '#ff00ff',
+            'stroke': '#aaaaaa',
+            'stroke_width': 0.01,
+        },
+        'children': [
+            {
+                'visual': {
+                    'name': 'Rectangle',
+                    'width': 0.05,
+                    'height': 0.1,
+                    'fill': '#00ff00',
+                    'stroke': '#0000ff',
+                    'stroke_width': 0.003,
+                },
+                'position': (0.125, 0, 1)
+            },
+            {
+                'visual': {
+                    'name': 'Rectangle',
+                    'width': 0.05,
+                    'height': 0.1,
+                    'stroke': '#ff0000',
+                    'stroke_width': 0.003,
+                },
+                'position': (-0.125, 0, 1)
+            },
+        ]
+    }
+
     def startUp(self):
-        self.rect = Rectangle(height=0.15, width=0.3, stroke='#ff0000')
-        self.rect2 = Rectangle(height=0.15, width=0.3, fill='#00ff00', stroke='#0000ff')
-        self.circle = Circle(radius=0.15, fill='#ff00ff', stroke='#aaaaaa', stroke_width=0.01)
-        ScreenObjectManager.instance.registerObject(self.rect, 'testingRect')
-        ScreenObjectManager.instance.registerObject(self.rect2, 'testingRect2')
-        ScreenObjectManager.instance.registerObject(self.circle, 'testingCircle')
+        self.robot = objectFactory(**self.ROBOT_DEFINITION)
+        ScreenObjectManager.instance.registerObject(self.robot, 'testingRobot')
 
     def tick(self, tick):
         x = tick / 2000
-        self.circle.position = (
-            0.5 + np.cos(-0.3*x) / 10,
-            0.5 + np.sin(-0.3*x) / 10,
-            2
-        )
-        self.rect.rotation = x
-        self.rect.position = (
+        self.robot.rotation = x
+        self.robot.position = (
             0.5 + np.cos(0.3*x) / 4,
             0.5 + np.sin(0.3*x) / 4,
             1
-        )
-        self.rect2.rotation = x + np.pi / 2
-        self.rect2.position = (
-            0.5 + np.cos(0.3*x) / 4,
-            0.5 + np.sin(0.3*x) / 4,
-            0
         )
         return x >= 4 * np.pi / 0.3
