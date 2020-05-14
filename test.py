7a276a44a0f1a0c93ef09559172b6be4153493d1
Jackson Goerner <37640160+glipR@users.noreply.github.com>
2020-05-14 13:12:07 +1000

    Add manager methods for registering objects.

--- test.py
+++ test.py
@@ -6,6 +6,10 @@ man = ScreenObjectManager()
 man.start_screen()
 
 rect = Rectangle(height=0.15, width=0.3, stroke='#ff0000')
+rect2 = Rectangle(height=0.15, width=0.3, fill='#00ff00', stroke='#0000ff')
+man.registerObject(rect, 'testingRect')
+man.registerObject(rect2, 'testingRect2')
+
 
 import numpy as np
 import time
@@ -15,12 +19,16 @@ for x in np.arange(0, 6*np.pi, step=0.1):
     rect.position = (
         0.5 + np.cos(0.3*x) / 4,
         0.5 + np.sin(0.3*x) / 4,
+        1
+    )
+    rect2.rotation = x + np.pi / 2
+    rect2.position = (
+        0.5 + np.cos(0.3*x) / 4,
+        0.5 + np.sin(0.3*x) / 4,
         0
     )
+    man.applyToScreen()
     time.sleep(0.02)
-    rect.apply_to_screen()
-    pygame.display.update()
-    man.screen.fill((0, 0, 0))
 
 finished = False
 while not finished:
