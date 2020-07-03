620db4429e6f11779833c18d1285918960e585ed
glipR <37640160+glipR@users.noreply.github.com>
2020-07-03 15:54:13 +1000

    Add world for checking collisions and modify test.

---
+++ simulation/world.py
@@ -0,0 +1,38 @@
+import pygame
+from objects.base import PhysicsObject
+from visual.manager import ScreenObjectManager
+from visual.utils import worldspace_to_screenspace
+
+class World:
+
+    instance: 'World' = None
+
+    def __init__(self):
+        World.instance = self
+        self.objects = []
+    
+    def registerObject(self, obj: PhysicsObject):
+        self.objects.append(obj)
+    
+    def tick(self, dt):
+        # TODO: Add some options here, and do physics.
+        # I just check for collisions and scream if this is the case.
+        for i, obj in enumerate(self.objects):
+            for obj2 in self.objects[i+1:]:
+                res = obj.collider.getCollisionInfo(obj2.collider)
+                if res['collision']:
+                    print("Colliding!")
+                    from visual.objects import visualFactory
+                    p = res['world_space_position']
+                    p[2] = 10
+                    obj = visualFactory(**{
+                        'name': 'Circle',
+                        'radius': 3,
+                        'fill': '#ffffff',
+                        'stroke_width': 0,
+                        'position': p
+                    })
+                    if 'point' in ScreenObjectManager.instance.objects:
+                        ScreenObjectManager.instance.unregisterVisual('point')
+                    ScreenObjectManager.instance.registerVisual(obj, 'point')
+                    
--- tests/random_testing.py
+++ tests/random_testing.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from simulation.interactor import IInteractor
 from simulation.loader import ScriptLoader
+from simulation.world import World
 from objects.base import objectFactory
 from visual import ScreenObjectManager
 
@@ -53,14 +54,15 @@ class RandomInteractor(IInteractor):
 
     def startUp(self, **kwargs):
         self.robot = objectFactory(**self.options)
+        World.instance.registerObject(self.robot)
         ScreenObjectManager.instance.registerObject(self.robot, self.key)
 
     def tick(self, tick):
         x = 3 * tick / self.constants[ScriptLoader.KEY_TICKS_PER_SECOND]
         self.robot.rotation = x
         self.robot.position = (
-            np.cos(0.3*x + self.offset * 2 * np.pi) * 30,
-            np.sin(0.3*x + self.offset * 2 * np.pi) * 30,
+            np.cos(0.3*x + self.offset * 2 * np.pi) * (30-x),
+            np.sin(0.3*x + self.offset * 2 * np.pi) * (30-x),
             1
         )
         return x >= 4 * np.pi / 0.3
