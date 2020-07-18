b2d009c63f82e2e1f45792c06fd20b77b98e078f
glipR <37640160+glipR@users.noreply.github.com>
2020-07-18 12:44:30 +1000

    Add example of soccer robot.

---
+++ robot/examples/soccer_bot.py
@@ -0,0 +1,17 @@
+from robot import Robot
+from devices.motor.ev3 import LargeMotor
+from devices.ultrasonic.ev3 import UltrasonicSensor
+
+class SoccerBot(Robot):
+
+    def startUp(self):
+        self.leftMotor : LargeMotor = self.getDevice('outB')
+        self.rightMotor : LargeMotor = self.getDevice('outC')
+        self.ultrasonic : UltrasonicSensor = self.getDevice('in3')
+    
+    def validateSpeed(self, speed):
+        return max(min(speed, 100), -100)
+
+    def tick(self, _):
+        self.leftMotor.on(self.validateSpeed(10*(self.ultrasonic.distance_centimeters - 60)))
+        self.rightMotor.on(self.validateSpeed(10*(self.ultrasonic.distance_centimeters - 60)))
