7e618e97a7a317a3a4de6016577e733ad62daa86
glipR <37640160+glipR@users.noreply.github.com>
2020-07-19 12:23:30 +1000

    Add bot spawning capabilities.

--- presets/soccer.py
+++ presets/soccer.py
@@ -1,3 +1,4 @@
+import numpy as np
 from simulation.interactor import IInteractor
 from simulation.loader import ScriptLoader
 from objects.base import objectFactory
@@ -5,6 +6,8 @@ from visual.manager import ScreenObjectManager
 
 class SoccerInteractor(IInteractor):
     
+    BOTS_PER_TEAM = 1
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.names = kwargs.get('names', ['Team 1', 'Team 2'])
@@ -17,6 +20,7 @@ class SoccerInteractor(IInteractor):
         # Initialise the goal colliders.
         self.goal_colliders = []
         self.team_scores = []
+        self.robots = []
         for x in range(len(self.names)):
             # Set up goal collider.
             pos = self.goals[x]['position']
@@ -33,11 +37,30 @@ class SoccerInteractor(IInteractor):
             self.team_scores.append(0)
             # Set up team name
             ScriptLoader.instance.object_map[f'name{x+1}Text'].text = self.names[x]
+            for y in range(self.BOTS_PER_TEAM):
+                # Find the BOTS_PER_TEAM*x+yth robot.
+                possible_keys = []
+                for key in ScriptLoader.instance.object_map.keys():
+                    if key.startswith(f'Robot-{self.BOTS_PER_TEAM*x+y}'):
+                        possible_keys.append(key)
+                if len(possible_keys) == 0:
+                    raise ValueError(f"No Robot-{self.BOTS_PER_TEAM*x+y} for simulation, quitting.")
+                possible_keys.sort(key=len)
+            self.robots.append(ScriptLoader.instance.object_map[possible_keys[0]])
         self.updateScoreText()
+        self.resetPositions()
 
     def updateScoreText(self):
         for x in range(len(self.names)):
             ScriptLoader.instance.object_map[f'score{x+1}Text'].text = str(self.team_scores[x])
+
+    def resetPositions(self):
+        # It is assumed that 2 robots to each team, with indexes increasing as we go across teams.
+        for team in range(len(self.names)):
+            for index in range(self.BOTS_PER_TEAM):
+                self.robots[team*self.BOTS_PER_TEAM + index].position = self.spawns[team][index][0]
+                self.robots[team*self.BOTS_PER_TEAM + index].rotation = self.spawns[team][index][1] * np.pi / 180
+        ScriptLoader.instance.object_map['IR_BALL'].position = [0, -18]
 
     def tick(self, tick):
         super().tick(tick)
