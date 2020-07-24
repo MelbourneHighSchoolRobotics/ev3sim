61f6176c14d126e61a0bb2abdd54c22483239083
glipR <37640160+glipR@users.noreply.github.com>
2020-07-24 16:35:39 +1000

    Remove unnecessary motor code.

--- devices/motor/ev3.py
+++ devices/motor/ev3.py
@@ -15,18 +15,6 @@ class MotorInteractor(IDeviceInteractor):
             return 'LMotor'
         return 'MMotor'
 
-    def handleEvent(self, event):
-        if event.type == pygame.KEYDOWN:
-            if event.key == self.device_class.F_k:
-                self.device_class.on(100)
-            if event.key == self.device_class.B_k:
-                self.device_class.on(-100)
-        if event.type == pygame.KEYUP:
-            if event.key == self.device_class.F_k and self.device_class.applied_force > 0:
-                self.device_class.off()
-            if event.key == self.device_class.B_k and self.device_class.applied_force < 0:
-                self.device_class.off()
-
     def tick(self, tick):
         self.device_class._updateTime(tick)
         if self.device_class.applied_force > 0:
@@ -39,11 +27,7 @@ class MotorInteractor(IDeviceInteractor):
 class LargeMotor(Device, MotorMixin):
 
     MAX_FORCE = 100000
-    F_k = pygame.K_f
-    B_k = pygame.K_v
 
 class MediumMotor(Device, MotorMixin):
 
     MAX_FORCE = 50000
-    F_k = pygame.K_j
-    B_k = pygame.K_n
