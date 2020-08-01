1c6c70d1c2fe968281c4fc13b6c5874d6d7fe6a4
glipR <37640160+glipR@users.noreply.github.com>
2020-08-01 18:08:28 +1000

    Add ultrasonic documentation.

--- devices/ultrasonic/ev3.py
+++ devices/ultrasonic/ev3.py
@@ -20,7 +20,7 @@ class UltrasonicInteractor(IDeviceInteractor):
 
     def tick(self, tick):
         if tick % (ScriptLoader.instance.GAME_TICK_RATE // self.UPDATE_PER_SECOND) == 0:
-            self.device_class.calc()
+            self.device_class._calc()
             ScriptLoader.instance.object_map[self.getPrefix() + 'light_up'].visual.fill = (
                 min(max((self.device_class.MAX_RAYCAST - self.device_class.distance_centimeters) * 255 / self.device_class.MAX_RAYCAST, 0), 255),
                 0,
@@ -29,6 +29,11 @@ class UltrasonicInteractor(IDeviceInteractor):
         return False
 
 class UltrasonicSensor(Device, UltrasonicSensorMixin):
+    """
+    Ultrasonic sensor, reads the distance between the sensor and the closest physics object (directly in front of the sensor).
+
+    This measurement is done from the light on the sensor, so a reading of 5cm means the closest object is 5cm away from the light.
+    """
 
     name = 'Ultrasonic'
 
@@ -37,13 +42,19 @@ class UltrasonicSensor(Device, UltrasonicSensorMixin):
         self._SetIgnoredObjects([parent])
         self._InitialiseRaycast()
 
-    def calc(self):
+    def _calc(self):
         self.saved = self._DistanceFromSensor(ScriptLoader.instance.object_map[self._interactor.getPrefix() + 'light_up'].position, self.parent.rotation + self.relativeRot)
     
     @property
     def distance_centimeters(self):
+        """
+        Get the distance between the ultrasonic sensor and the object, in centimeters.
+        """
         return self.saved
     
     @property
     def distance_inches(self):
-        raise NotImplementedError("`distance_inches` is currently not implemented.")
+        """
+        Get the distance between the ultrasonic sensor and the object, in inches.
+        """
+        return self.distance_centimeters * 0.3937008
