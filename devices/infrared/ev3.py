01e7b516c02a56b1389f466298e2c6cb056503f1
glipR <37640160+glipR@users.noreply.github.com>
2020-08-01 17:41:20 +1000

    Add infrared documentation.

--- devices/infrared/ev3.py
+++ devices/infrared/ev3.py
@@ -18,17 +18,50 @@ class InfraredInteractor(IDeviceInteractor):
         distance = np.sqrt(magnitude_sq(ball_pos - sensor.position))
         vector = ball_pos - sensor.position
         relative_bearing = np.arctan2(vector[1], vector[0]) - sensor.rotation
-        self.device_class.calc(relative_bearing, distance)
+        self.device_class._calc(relative_bearing, distance)
         for x in range(5):
             ScriptLoader.instance.object_map[self.getPrefix() + f'light_up_{x}'].visual.fill = (max(min(255 * self.device_class.value(x+1) / 9, 255), 0), 0, 0)
         return False
 
 class InfraredSensor(Device, InfraredSensorMixin):
+    """
+    Infrared Sensor can detect the soccer ball in a cone of vision, and has 5 small sensors which aggregate to localise the ball.
+    (More generally, it can detect any physical object with tag 'IR_BALL')
 
-    def calc(self, relativeBearing, distance):
+    It has one method, `value`, whose input can be 0-6, returning different sensor data.
+    """
+
+    def _calc(self, relativeBearing, distance):
         self._values = self._sensorValues(relativeBearing, distance)
     
     def value(self, index):
+        """
+        Get sensor data.
+        
+        index=0:
+            Get a crude direction prediction from the sensor.
+            return 0: Ball lost
+            return 1-9: Ball is (1: Far left) (...) (5: Centred) (...) (9: Far right)
+        index=1-5:
+            Get the sensor value on subsensor 1-5. Has a value ranging from 0-9.
+            Sensor 1: Far left
+            Sensor 2: Left
+            Sensor 3: Middle
+            Sensor 4: Right
+            Sensor 5: Far right
+        index=6:
+            Get the average sensor value.
+
+        Example usage:
+        ```
+        >>> print([ir.value(x) for x in range(1, 6)])
+        [0, 4, 8, 4, 0]
+        >>> print(ir.value(0))
+        5
+        >>> print(ir.value(6))
+        3.2
+        ```
+        """
         if index == 0:
             return self._predict(self._values)
         if 1 <= index <= 5:
