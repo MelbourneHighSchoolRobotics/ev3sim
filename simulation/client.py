123067ac587f60d9b618bb0bc56b7d888eabc2c2
glipR <37640160+glipR@users.noreply.github.com>
2020-08-06 17:32:06 +1000

    Ensure client crashes silently close communication threads.

--- simulation/client.py
+++ simulation/client.py
@@ -36,103 +36,107 @@ def write(data):
             stub.SendWriteInfo(simulation.comm_schema_pb2.RobotWrite(robot_id=robot_id, attribute_path=path, value=value))
         response = stub.RequestTickUpdates(simulation.comm_schema_pb2.RobotRequest(robot_id=robot_id))
 
-def robot(filename, data):
+def robot(filename, data, result):
+    try:
+        from ev3dev2 import Device, DeviceNotFound
 
-    from ev3dev2 import Device, DeviceNotFound
-
-    class MockedFile:
-        def __init__(self, data_path):
-            self.k2, self.k3, self.k4 = data_path
-            self.seek_point = 0
-        
-        def read(self):
-            if isinstance(data['current_data'][self.k2][self.k3][self.k4], int):
-                res = str(data['current_data'][self.k2][self.k3][self.k4])
-            if isinstance(data['current_data'][self.k2][self.k3][self.k4], str):
-                if self.seek_point == 0:
-                    res = data['current_data'][self.k2][self.k3][self.k4]
-                else:
-                    res = data['current_data'][self.k2][self.k3][self.k4][self.seek_point:]
-            return res.encode('utf-8')
-        
-        def seek(self, i):
-            self.seek_point = i
-        
-        def write(self, value):
-            data['actions_queue'].put((f'{self.k2} {self.k3} {self.k4}', value.decode()))
-            print(f"Wrote {self.k2} {self.k3} {self.k4} {value}")
-        
-        def flush(self):
-            pass
-
-    def device__init__(self, class_name, name_pattern='*', name_exact=False, **kwargs):
-        print("Initialising device!")
-        self._path = [class_name]
-        self.kwargs = kwargs
-        self._attr_cache = {}
-
-        def get_index(file):
-            match = Device._DEVICE_INDEX.match(file)
-            if match:
-                return int(match.group(1))
-            else:
-                return None
-        
-        if name_exact:
-            self._path.append(name_pattern)
-            self._device_index = get_index(name_pattern)
-        else:
-            for name in data['current_data'][self._path[0]].keys():
-                for k in kwargs:
-                    if k not in data['current_data'][self._path[0]][name]:
-                        break
-                    if isinstance(kwargs[k], list):
-                        if data['current_data'][self._path[0]][name][k] not in kwargs[k]:
-                            break
+        class MockedFile:
+            def __init__(self, data_path):
+                self.k2, self.k3, self.k4 = data_path
+                self.seek_point = 0
+            
+            def read(self):
+                if isinstance(data['current_data'][self.k2][self.k3][self.k4], int):
+                    res = str(data['current_data'][self.k2][self.k3][self.k4])
+                if isinstance(data['current_data'][self.k2][self.k3][self.k4], str):
+                    if self.seek_point == 0:
+                        res = data['current_data'][self.k2][self.k3][self.k4]
                     else:
-                        if data['current_data'][self._path[0]][name][k] != kwargs[k]:
-                            break
+                        res = data['current_data'][self.k2][self.k3][self.k4][self.seek_point:]
+                return res.encode('utf-8')
+            
+            def seek(self, i):
+                self.seek_point = i
+            
+            def write(self, value):
+                data['actions_queue'].put((f'{self.k2} {self.k3} {self.k4}', value.decode()))
+                print(f"Wrote {self.k2} {self.k3} {self.k4} {value}")
+            
+            def flush(self):
+                pass
+
+        def device__init__(self, class_name, name_pattern='*', name_exact=False, **kwargs):
+            print("Initialising device!")
+            self._path = [class_name]
+            self.kwargs = kwargs
+            self._attr_cache = {}
+
+            def get_index(file):
+                match = Device._DEVICE_INDEX.match(file)
+                if match:
+                    return int(match.group(1))
                 else:
-                    self._path.append(name)
-                    self._device_index = get_index(name)
-                    break
+                    return None
+            
+            if name_exact:
+                self._path.append(name_pattern)
+                self._device_index = get_index(name_pattern)
             else:
-                print(kwargs, data['current_data'][self._path[0]])
-                self._device_index = None
+                for name in data['current_data'][self._path[0]].keys():
+                    for k in kwargs:
+                        if k not in data['current_data'][self._path[0]][name]:
+                            break
+                        if isinstance(kwargs[k], list):
+                            if data['current_data'][self._path[0]][name][k] not in kwargs[k]:
+                                break
+                        else:
+                            if data['current_data'][self._path[0]][name][k] != kwargs[k]:
+                                break
+                    else:
+                        self._path.append(name)
+                        self._device_index = get_index(name)
+                        break
+                else:
+                    print(kwargs, data['current_data'][self._path[0]])
+                    self._device_index = None
 
-                raise DeviceNotFound("%s is not connected." % self)
+                    raise DeviceNotFound("%s is not connected." % self)
 
-    def _attribute_file_open(self, name):
-        return MockedFile((self._path[0], self._path[1], name))
+        def _attribute_file_open(self, name):
+            return MockedFile((self._path[0], self._path[1], name))
 
-    def wait(self, cond, timeout=None):
-        import time
-        tic = time.time()
-        if cond(self.state):
-            return True
-        # Register to active_data_handlers so we can do something every tick without lagging.
-        handler_key = ' ' .join(self._path)
-        data['active_data_handlers'][handler_key] = Queue(maxsize=0)
-        while True:
-            data['active_data_handlers'][handler_key].get()
-            res = cond(self.state)
-            if res or ((timeout is not None) and (time.time() >= tic + timeout / 1000)):
-                del data['active_data_handlers'][handler_key]
-                return cond(self.state)
+        def wait(self, cond, timeout=None):
+            import time
+            tic = time.time()
+            if cond(self.state):
+                return True
+            # Register to active_data_handlers so we can do something every tick without lagging.
+            handler_key = ' ' .join(self._path)
+            data['active_data_handlers'][handler_key] = Queue(maxsize=0)
+            while True:
+                data['active_data_handlers'][handler_key].get()
+                res = cond(self.state)
+                if res or ((timeout is not None) and (time.time() >= tic + timeout / 1000)):
+                    del data['active_data_handlers'][handler_key]
+                    return cond(self.state)
 
-    def get_time():
-        return data['tick'] / data['tick_rate']
+        def get_time():
+            return data['tick'] / data['tick_rate']
 
-    @mock.patch('time.time', mock.MagicMock(side_effect=get_time))
-    @mock.patch('ev3dev2.motor.Motor.wait', wait)
-    @mock.patch('ev3dev2.Device.__init__', device__init__)
-    @mock.patch('ev3dev2.Device._attribute_file_open', _attribute_file_open)
-    def run_script(fname):
-        from importlib.machinery import SourceFileLoader
-        module = SourceFileLoader('__main__', fname).load_module()
-    
-    assert data['start_robot_queue'].get(), "Something went wrong..."
-    run_script(filename)
+        @mock.patch('time.time', mock.MagicMock(side_effect=get_time))
+        @mock.patch('ev3dev2.motor.Motor.wait', wait)
+        @mock.patch('ev3dev2.Device.__init__', device__init__)
+        @mock.patch('ev3dev2.Device._attribute_file_open', _attribute_file_open)
+        def run_script(fname):
+            from importlib.machinery import SourceFileLoader
+            module = SourceFileLoader('__main__', fname).load_module()
+        
+        assert data['start_robot_queue'].get(), "Something went wrong..."
+        run_script(filename)
+    except Exception as e:
+        result.put(e)
+        return
+    result.put(True)
 
 shared_data = {
     'tick': 0,
@@ -143,15 +147,22 @@ shared_data = {
     'active_data_handlers': {},
 }
 
+result_bucket = Queue(maxsize=1)
+
 from threading import Thread
 
 comm_thread = Thread(target=comms, args=(shared_data,), daemon=True)
-robot_thread = Thread(target=robot, args=(sys.argv[1], shared_data,))
+robot_thread = Thread(target=robot, args=(sys.argv[1], shared_data, result_bucket,))
 write_thread = Thread(target=write, args=(shared_data,), daemon=True)
 
 comm_thread.start()
 write_thread.start()
 robot_thread.start()
 
-robot_thread.join()
-time.sleep(1)
+try:
+    r = result_bucket.get()
+    if r is not True:
+        raise r
+    time.sleep(1)
+except KeyboardInterrupt:
+    pass
