1ff2488eff4e0180e4531064d40e0484e7810696
glipR <37640160+glipR@users.noreply.github.com>
2020-08-16 14:22:36 +1000

    Add invisible walls and add collider to play/pause UI.

--- ev3sim/presets/pause_interactor.py
+++ ev3sim/presets/pause_interactor.py
@@ -12,10 +12,14 @@ class PauseInteractor(IInteractor):
             World.instance.paused = not World.instance.paused
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             m_pos = screenspace_to_worldspace(event.pos)
-            # Click event for object['controls'] should go here
+            # TODO: Add a UI Mask for colliders.
+            shapes = World.instance.space.point_query(m_pos, 0.0, pymunk.ShapeFilter(mask=pymunk.ShapeFilter.ALL_MASKS))
+            if len(shapes) > 0 and shapes[0].shape.obj.key == 'controls':
+                # Toggle pause state.
+                World.instance.paused = not World.instance.paused
     
     def tick(self, tick):
         if World.instance.paused:
-            ScriptLoader.instance.object_map["controls"].image_path = 'assets/ui/controls_pause.png'
+            ScriptLoader.instance.object_map["controls"].visual.image_path = 'assets/ui/controls_pause.png'
         else: 
-            ScriptLoader.instance.object_map["controls"].image_path = 'assets/ui/controls_play.png'
+            ScriptLoader.instance.object_map["controls"].visual.image_path = 'assets/ui/controls_play.png'
