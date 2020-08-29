0c19e4da3194389b4878dc1dc606eb567fc8c0a3
glipR <37640160+glipR@users.noreply.github.com>
2020-08-29 16:43:12 +1000

    Ensure screenobject manager keeps relative size saved.

--- ev3sim/visual/manager.py
+++ ev3sim/visual/manager.py
@@ -26,6 +26,8 @@ class ScreenObjectManager:
     def initFromKwargs(self, **kwargs):
         self.screen_width = kwargs.get('screen_width', 640)
         self.screen_height = kwargs.get('screen_height', 480)
+        self.original_screen_width = self.screen_width
+        self.original_screen_height = self.screen_height
         # NOTE: TEMPORARY - this would describe the dimensions of the simulated map vs the screen dimensions.
         self.map_width = kwargs.get('map_width', 210)
         self.map_height = kwargs.get('map_height', 160)
@@ -110,3 +112,10 @@ class ScreenObjectManager:
                 pygame.quit()
                 ScriptLoader.instance.running = False
             yield event
+
+    def relativeScreenScale(self):
+        """Returns the relative scaling of the screen that has occur since the screen was first initialised."""
+        return [
+            self.screen_width / self.original_screen_width,
+            self.screen_height / self.original_screen_height,
+        ]
