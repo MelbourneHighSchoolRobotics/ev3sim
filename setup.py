d8c629bf68c05687568f2e6cf3957563a460a67f
Peter Drew <peter-drew@users.noreply.github.com>
2020-08-30 12:40:03 +1000

    bump version to 1.2.1

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open(os.path.join(HERE, "requirements.txt")) as fid:
 
 setup(
     name="ev3sim",
-    version="1.2.0",
+    version="1.2.1",
     description="Simulate ev3dev programs in Python",
     long_description=README,
     long_description_content_type="text/markdown",
