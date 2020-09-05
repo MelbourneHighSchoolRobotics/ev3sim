ddc6d18f07d11f362a8a6092363615dfdc874df5
glipR <37640160+glipR@users.noreply.github.com>
2020-09-05 14:53:54 +1000

    Add utilities for more nearest value randomisation.

--- ev3sim/devices/utils.py
+++ ev3sim/devices/utils.py
@@ -1,3 +1,6 @@
+import numpy as np
+from ev3sim.simulation.randomisation import Randomiser
+
 class NearestValue:
     """
     Utility class to support defaulting to the nearest value to some input, with points equidistantly separated between max and min.
@@ -29,3 +32,49 @@ class NearestValue:
         if abs(self.points[lo] - val) > abs(self.points[lo+1] - val):
             return self.points[lo+1]
         return self.points[lo]
+
+class CyclicMixin:
+    """
+    Nearest Value, but compare points modulo some value.
+
+    Assumes that minimum and maximum represent the same value and they are cyclic about this point.
+    """
+
+    def get_closest(self, val):
+        while val < self.points[0]:
+            val += (self.points[-1] - self.points[0])
+        while val > self.points[-1]:
+            val -= (self.points[-1] - self.points[0])
+        # points[0] <= val <= points[-1]
+        # Ensure that we don't return points[-1], handle this separately
+        if abs(self.points[-2] - val) > abs(self.points[-1] - val):
+            return self.points[0]
+        # Binary search to find largest index which houses a smaller value.
+        lo = 0
+        hi = len(self.points)
+        while hi - lo > 1:
+            mid = (hi + lo) // 2
+            if self.points[mid] > val:
+                hi = mid
+            else:
+                lo = mid
+        if abs(self.points[lo] - val) > abs(self.points[lo+1] - val):
+            return self.points[lo+1]
+        return self.points[lo]
+    
+class RandomDistributionMixin:
+
+    def __init__(self, minimum, maximum, num_points, distribution_var):
+        """Generate the points using a normal distribution"""
+        super().__init__(minimum, maximum, num_points)
+        # Redefine self.points
+        step = (self.max - self.min) / (num_points - 1)
+        steps = Randomiser.getGlobalRandom().normal(loc=step, scale=np.sqrt(distribution_var), size=(num_points-2, ))
+        self.points = [self.min]
+        cur_val = self.min
+        for step in steps:
+            cur_val += step
+            cur_val = min(max(cur_val, self.min), self.max)
+            self.points.append(cur_val)
+        self.points.append(self.max)
+        self.points.sort()
