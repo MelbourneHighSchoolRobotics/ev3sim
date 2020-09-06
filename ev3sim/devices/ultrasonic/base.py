cf2fc46800a0bae1600d6c330ad1c84538bf875c
glipR <37640160+glipR@users.noreply.github.com>
2020-09-06 10:15:04 +1000

    Add ultrasonic randomisation.

--- ev3sim/devices/ultrasonic/base.py
+++ ev3sim/devices/ultrasonic/base.py
@@ -2,6 +2,8 @@ import numpy as np
 import pymunk
 from ev3sim.objects.base import objectFactory
 from ev3sim.simulation.world import World
+from ev3sim.simulation.loader import ScriptLoader
+from ev3sim.simulation.randomisation import Randomiser
 
 class UltrasonicSensorMixin:
 
@@ -15,6 +17,11 @@ class UltrasonicSensorMixin:
     MAX_RAYCAST = 120
     # The maximum level of discrepancy to reality
     ACCEPTANCE_LEVEL = 1
+
+    # 0 - 2 of actual value at max angle.
+    ANGLE_RANDOM_AMPLITUDE = 1
+
+    last_angle_diff = 0
 
     def _SetIgnoredObjects(self, objs):
         self.ignore_objects = objs
@@ -37,7 +44,19 @@ class UltrasonicSensorMixin:
                     i += 1
 
             if raycast == None:
-                return top_length
+                if top_length == self.MAX_RAYCAST or (not ScriptLoader.RANDOMISE_SENSORS):
+                    return top_length
+                # If randomiser, linearly scale result by angle between surface normal of raycasted point.
+                return max(0, min(self.MAX_RAYCAST, 
+                    top_length * (1 + self.last_angle_diff * (Randomiser.random() - 0.5) * self.ANGLE_RANDOM_AMPLITUDE / np.pi * 2)
+                ))
+            else:
+                opposite_angle = centreRotation + np.pi
+                while opposite_angle > raycast.normal.angle + np.pi:
+                    opposite_angle -= 2*np.pi
+                while opposite_angle < raycast.normal.angle - np.pi:
+                    opposite_angle += 2*np.pi
+                self.last_angle_diff = abs(opposite_angle - raycast.normal.angle)
             top_length = raycast.alpha * top_length - self.ACCEPTANCE_LEVEL
         return 0                
 
