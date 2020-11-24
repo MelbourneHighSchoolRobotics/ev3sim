d196031df18c86a3102e0bbfb79a3bd0404265da
glipR <37640160+glipR@users.noreply.github.com>
2020-11-25 10:04:25 +1100

    Add file selector.

--- ev3sim/visual/settings/elements.py
+++ ev3sim/visual/settings/elements.py
@@ -33,9 +33,86 @@ class SettingsVisualElement:
     def resize(self, objects, index):
         raise NotImplementedError()
 
-    def generateVisual(self, idx):
+    def generateVisual(self, relative_rect, container, manager, idx):
         raise NotImplementedError()
 
+
+class FileEntry(SettingsVisualElement):
+
+    num_objs = 4
+
+    def generateVisual(self, relative_rect, container, manager, idx):
+        self.container = container
+        label = pygame_gui.elements.UILabel(
+            relative_rect=relative_rect,
+            manager=manager,
+            object_id=pygame_gui.core.ObjectID(f"{idx}-file-label", "entry-label"),
+            container=container,
+            text=self.title,
+        )
+        self.filename = pygame_gui.elements.UILabel(
+            relative_rect=relative_rect,
+            manager=manager,
+            object_id=pygame_gui.core.ObjectID(f"{idx+1}-file-name", "entry-label"),
+            container=container,
+            text=self.current,
+        )
+        click = pygame_gui.elements.UIButton(
+            relative_rect=relative_rect,
+            manager=manager,
+            object_id=pygame_gui.core.ObjectID(f"{idx+2}-button", "file-button"),
+            container=container,
+            text="",
+        )
+        click_icon = pygame_gui.elements.UIImage(
+            relative_rect=relative_rect,
+            manager=manager,
+            object_id=pygame_gui.core.ObjectID(f"{idx+3}-image", "file-image"),
+            container=container,
+            image_surface=pygame.Surface((relative_rect.width, relative_rect.height)),
+        )
+        return [label, self.filename, click, click_icon]
+
+    def resize(self, objs, index):
+        off = self.offset((self.container.relative_rect.width, self.container.relative_rect.height))
+        button_size = ((self.container.relative_rect.width - 40) * 0.25 - 20, 44)
+        button_size = [min(button_size[0], button_size[1])] * 2
+        label_size = ((self.container.relative_rect.width - 40) / 2 - 10, 40)
+        file_size = ((self.container.relative_rect.width - 40) / 2 - button_size[0] - 30, 40)
+        objs[index].set_dimensions(label_size)
+        objs[index+1].set_dimensions(file_size)
+        objs[index+2].set_dimensions(button_size)
+        objs[index+3].set_dimensions(button_size)
+
+        objs[index].set_position(
+            (off[0] + self.container.relative_rect.left + 20, off[1] + self.container.relative_rect.top)
+        )
+        objs[index + 1].set_position(
+            (off[0] + self.container.relative_rect.left + 40 + label_size[0], off[1] + self.container.relative_rect.top)
+        )
+        objs[index + 2].set_position(
+            (off[0] + self.container.relative_rect.left + 50 + label_size[0] + file_size[0], off[1] + self.container.relative_rect.top - 2)
+        )
+        objs[index + 3].set_position(
+            (off[0] + self.container.relative_rect.left + 50 + label_size[0] + file_size[0], off[1] + self.container.relative_rect.top - 2)
+        )
+        img = pygame.image.load(
+            find_abs("ui/folder.png", allowed_areas=["package/assets/"])
+        )
+        if img.get_size() != objs[index + 3].rect.size:
+            img = pygame.transform.smoothscale(img, (objs[index + 3].rect.width, objs[index + 3].rect.height))
+        objs[index + 3].set_image(img)
+
+    def handlePressed(self, idx):
+        assert idx == 2, f"{idx} expected to be 2."
+        # Open file dialog.
+        from tkinter import Tk
+        from tkinter.filedialog import askdirectory
+
+        Tk().withdraw()
+        directory = askdirectory()
+        self.current = directory
+        self.filename.set_text(self.current)
 
 class TextEntry(SettingsVisualElement):
 
