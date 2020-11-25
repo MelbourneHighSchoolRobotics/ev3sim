620b3ca8bffad91e1e5adc32621cd2cee3643a1f
glipR <37640160+glipR@users.noreply.github.com>
2020-11-25 11:04:33 +1100

    Allow directory finder to create directories if necessary.

--- ev3sim/file_helper.py
+++ ev3sim/file_helper.py
@@ -65,5 +65,16 @@ def find_abs(filepath, allowed_areas=None):
     raise ValueError(f"File not found: {filepath}")
 
 
-def find_abs_directory(dirpath):
-    return find_abs("", allowed_areas=[dirpath])
+def find_abs_directory(dirpath, create=False):
+    try:
+        return find_abs("", allowed_areas=[dirpath])
+    except ValueError as e:
+        if not create:
+            raise e
+        else:
+            # Remove one part of the directory, then try again.
+            rest, single = os.path.split(dirpath.rstrip("/"))
+            dirname = find_abs_directory(rest, create=create)
+            fpath = os.path.join(dirname, single)
+            os.mkdir(fpath)
+            return fpath
