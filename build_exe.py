35afcffee73743d48b8bd21e148c6f260382db66
glipR <37640160+glipR@users.noreply.github.com>
2021-06-10 12:55:02 +1000

    Add instructions on including tkinter in installer.

--- build_exe.py
+++ build_exe.py
@@ -15,10 +15,26 @@ os.makedirs("dist", exist_ok=True)
 os.makedirs("dist/ev3sim", exist_ok=True)
 if os.path.exists("dist/python_embed"):
     shutil.rmtree("dist/python_embed")
-shutil.copytree("python_embed", "dist/python_embed")
+shutil.copytree("python_embed-32", "dist/python_embed")
 
 if os.path.exists("dist/ev3sim/user_config.yaml"):
     os.remove("dist/ev3sim/user_config.yaml")
 
 process = Popen("makensis config.nsi")
 process.wait()
+shutil.move("installer.exe", "installer-32bit.exe")
+
+if os.path.exists("dist"):
+    shutil.rmtree("dist")
+os.makedirs("dist", exist_ok=True)
+os.makedirs("dist/ev3sim", exist_ok=True)
+if os.path.exists("dist/python_embed"):
+    shutil.rmtree("dist/python_embed")
+shutil.copytree("python_embed-64", "dist/python_embed")
+
+if os.path.exists("dist/ev3sim/user_config.yaml"):
+    os.remove("dist/ev3sim/user_config.yaml")
+
+process = Popen("makensis config.nsi")
+process.wait()
+shutil.move("installer.exe", "installer-64bit.exe")
