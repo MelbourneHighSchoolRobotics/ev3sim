e4937f970b0b5490dbbcbc3e2318186d4db81746
James Bui <jtbui20@gmail.com>
2023-05-30 22:39:02 +1000

    Clean up installer build and entry point

--- build_exe.py
+++ build_exe.py
@@ -9,32 +9,22 @@ with open("version_file_template.txt", "r") as f:
 with open("version_file.txt", "w") as f:
     f.write(string)
 
-if os.path.exists("dist"):
-    shutil.rmtree("dist")
-os.makedirs("dist", exist_ok=True)
-os.makedirs("dist/ev3sim", exist_ok=True)
-if os.path.exists("dist/python_embed"):
-    shutil.rmtree("dist/python_embed")
-shutil.copytree("python_embed-32", "dist/python_embed")
+def generalBuild(arch): 
+    if os.path.exists("dist"):
+        shutil.rmtree("dist")
+    os.makedirs("dist", exist_ok=True)
 
-if os.path.exists("dist/ev3sim/user_config.yaml"):
-    os.remove("dist/ev3sim/user_config.yaml")
+    insertPythonEmbed(arch)
 
-process = Popen(["makensis", "config.nsi"])
-process.wait()
-shutil.move("installer.exe", "installer-32bit.exe")
+    process = Popen(["makensis", "config.nsi"])
+    process.wait()
+    shutil.move("installer.exe", f"installer-{arch}bit.exe")
 
-if os.path.exists("dist"):
-    shutil.rmtree("dist")
-os.makedirs("dist", exist_ok=True)
-os.makedirs("dist/ev3sim", exist_ok=True)
-if os.path.exists("dist/python_embed"):
-    shutil.rmtree("dist/python_embed")
-shutil.copytree("python_embed-64", "dist/python_embed")
+def insertPythonEmbed(arch):
+    if os.path.exists("dist/python_embed"):
+        shutil.rmtree("dist/python_embed")
+    shutil.copytree(f"python_embed-{arch}", "dist/python_embed")
 
-if os.path.exists("dist/ev3sim/user_config.yaml"):
-    os.remove("dist/ev3sim/user_config.yaml")
-
-process = Popen(["makensis", "config.nsi"])
-process.wait()
-shutil.move("installer.exe", "installer-64bit.exe")
+if __name__ == "__main__":
+    generalBuild(32)
+    generalBuild(64)
